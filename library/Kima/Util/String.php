edd839f00d31cb99b79e89a842a7d73c0f1c7281
Steavy Vega <steavy.vega@possibleworldwide.com>
2012-06-22 14:47:02 -0600

    Fixing function format

--- library/Kima/Util/String.php
+++ library/Kima/Util/String.php
@@ -15,7 +15,8 @@ class String
     /**
      * Transforms camel case to underscore
      */
-    public static function camel_case_to_underscore($string) {
+    public static function camel_case_to_underscore($string)
+    {
         return strtolower(preg_replace('/([a-z])([A-Z])/', '$1_$2', $string));
     }
 
