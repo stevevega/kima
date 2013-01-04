1e4eb0372428ca92ccabf4cd7d3c4e47308ebbf3
stevehuli <vega.saborio@gmail.com>
2013-01-04 15:07:20 -0600

    Adding example Bootstrap file

---
+++ application/Bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Kima Application Bootstrap
+ * @author Steve Vega
+ */
+
+/**
+ * Application Bootstrap
+ */
+class Bootstrap
+{
+
+    /**
+     * Test user bootstrap method
+     */
+    public function test()
+    {
+        // method code
+    }
+
+}
